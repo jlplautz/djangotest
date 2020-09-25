467e59ab13aff03eab30472cbb62bfd769c1b11c
61246880 <jorge.plautz@gmail.com>
2020-09-25 17:03:48 -0300

    Created pytest fixture
    close #9

--- products/tests/test_views.py
+++ products/tests/test_views.py
@@ -7,34 +7,30 @@ import pytest
 from products.views import product_detail
 
 
-@pytest.mark.django_db
-class TestViews(TestCase):
+@pytest.fixture
+def factory():
+    return RequestFactory()
 
-    @classmethod
-    def setUpClass(cls):
-        """
-        Esta função via criar um setup:
-        - esta é uma subclass do TestCase que vai garantir o acesso a metodos
-          chamados setup class. E tudo que for inserido nesta function será chamada
-          toda vez que outras funções
-        :return:
-        """
-        super(TestViews, cls).setUpClass()
-        mixer.blend('products.Product')
-        cls.factory = RequestFactory()
+@pytest.fixture
+def product(db):
+    return mixer.blend('products.Product')
 
-    def test_product_detail_authenticated(self):
-        path = reverse('detail', kwargs={'pk': 1})
-        request = self.factory.get(path)
-        request.user = mixer.blend(User)
 
-        response = product_detail(request, pk=1)
-        assert response.status_code == 200
+def test_product_detail_authenticated(factory, product):
 
-    def test_product_detail_unauthenticated(self):
-        path = reverse('detail', kwargs={'pk': 1})
-        request = self.factory.get(path)
-        request.user = AnonymousUser()
+    path = reverse('detail', kwargs={'pk': 1})
+    request = factory.get(path)
+    request.user = mixer.blend(User)
 
-        response = product_detail(request, pk=1)
-        assert 'accounts/login' in response.url
+    response = product_detail(request, pk=1)
+    assert response.status_code == 200
+
+
+def test_product_detail_unauthenticated(factory, product):
+
+    path = reverse('detail', kwargs={'pk': 1})
+    request = factory.get(path)
+    request.user = AnonymousUser()
+
+    response = product_detail(request, pk=1)
+    assert 'accounts/login' in response.url
